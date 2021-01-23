1eeec587218e71ab6bcdeac47a50cca5bcae5076
Maciej Szulc <224103@edu.p.lodz.pl>
2021-01-23 23:29:21 +0100

    add_graph fragment sketched

--- app/src/main/java/com/example/containcorona/fragments/AddGraphFragment.java
+++ app/src/main/java/com/example/containcorona/fragments/AddGraphFragment.java
@@ -1,11 +1,16 @@
 package com.example.containcorona.fragments;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
+import com.example.containcorona.GraphSettingsList;
 import com.example.containcorona.R;
 
 
@@ -16,5 +21,27 @@ public class AddGraphFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_graph, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (GraphSettingsList.pieNewVsTotalOn) {
+            CheckBox pie = getView().findViewById(R.id.pieCheck);
+            pie.setChecked(true);
+        }
+        if (GraphSettingsList.someOtherChartOn) {
+            CheckBox col = getView().findViewById(R.id.colCheck);
+            col.setChecked(true);
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        CheckBox pie = getView().findViewById(R.id.pieCheck);
+        CheckBox col = getView().findViewById(R.id.colCheck);
+        GraphSettingsList.pieNewVsTotalOn = pie.isChecked();
+        GraphSettingsList.someOtherChartOn = col.isChecked();
+        super.onDestroyView();
     }
 }
